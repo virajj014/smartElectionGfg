80e19fce7a38c163c2cea286239c9341b22f9cd5
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-05-25 20:39:48 +0530

    second

--- add_faces.py
+++ add_faces.py
@@ -6,7 +6,7 @@ import os
 if not os.path.exists('data/'):
     os.makedirs('data/')
 
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture(0)
 facedetect= cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 faces_data = []
 
